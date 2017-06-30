a690c1dbf41b7f790e31d00500d56401d0095976
Ramesh Hosamani <rhosmani@akamai.com>
2017-06-30 17:01:18 +0530

    Changes

--- music/urls.py
+++ music/urls.py
@@ -3,11 +3,20 @@ from . import views
 
 urlpatterns = [
     # /music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
-    # /music/<album_id>/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    #/music/register
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
+    # /music/<pk>/
+    # Detail view expects the primary key
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
-    # /music/<album_id>/favorites
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    #/music/album/add
+    url(r'^album/add/$', views.AlbumCreate.as_view(), name='Add-album'),
+
+    #/music/album/2/
+    url(r'^album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='Update-album'),
+
+    #/music/album/2/delete
+    url(r'^(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='Delete-album'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,76 +1,97 @@
-from django.http import HttpResponse
-from django.http import Http404
-from .models import Album, Song
-# Create your views here.
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from django.core.urlresolvers import reverse_lazy
+from .models import Album
+from .forms import UserForm
 
-'''
-def index(request):
-    #Connect to DataBase, look at albums table, and get all albums from there
-    all_albums = Album.objects.all()
-
-    html = ''
-    for album in all_albums:
-        url = '/music/' + str(album.id) + '/'
-        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
-    return HttpResponse(html)
-'''
-
-'''
-def index(request):
-    from django.template import loader
-    all_albums = Album.objects.all()
-
-    #By default django looks for templates directory in your app directory
-    template = loader.get_template('music/index.html')
-    context = {
-        'all_albums': all_albums,
-    }
-    return HttpResponse(template.render(context, request))
-'''
-
-
-def index(request):
-    from django.shortcuts import render
-    all_albums = Album.objects.all()
-
-#   By default django looks for templates directory in your app directory
-    context = {'all_albums': all_albums }
-
-
-#   render function will automatically send httpresponse as return
-    return render(request, 'music/index.html', context)
-
-
-def detail(request, album_id):
-    from django.shortcuts import render, get_object_or_404
+class IndexView(generic.ListView):
     '''
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
+        As in index we have list of albums, we are importing listview from generic, and creating class
     '''
-    album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'music/detail.html', {'album': album})
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
+
+    def get_queryset(self):
+        '''
+            Going to query the database for whatever albums we have and returns all the objects
+        :return:
+        '''
+        return Album.objects.all()
 
 
-def favorite(request, album_id):
-    from django.shortcuts import render, get_object_or_404
+class DetailView(generic.DetailView):
     '''
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
+        This DetailView will get details of one object
+        what model and what object you are trying to get details of need to mention
     '''
-    album = get_object_or_404(Album, pk=album_id)
-    try:
-        selected_song = album.song_set.get(pk=request.POST['song'])
-    except(KeyError, Song.DoesNotExist):
-        return render(request, 'music/detail.html', {
-            'album': album,
-            'error_message': "You did not select a valid song",
-        })
-    else:
-        selected_song.is_favourite = True
-        selected_song.save()
-        return render(request, 'music/detail.html', {'album': album})
 
+    model = Album
+    template_name = 'music/detail.html'
+
+class AlbumCreate(CreateView):
+    '''
+        It will create the new Album entry.
+    '''
+
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+
+class AlbumUpdate(UpdateView):
+    '''
+        It will Update the existing Album entry.
+    '''
+
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+
+class AlbumDelete(DeleteView):
+    '''
+        It will delete the existing Album.
+    '''
+
+    model = Album
+
+    #It will take you to the home page after deleting an object.
+    success_url = reverse_lazy('music:index')
+
+class UserFormView(View):
+    '''What is the blue print of the form'''
+    form_class = UserForm
+    template_name = 'music/registration_form.html'
+
+    #display blank form
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    #Fill the form and post it. process form data
+    def post(self, request):
+        form = self.form_class(request.Post)
+
+        if form.is_valid():
+            #It creates an object from the form, and it doesn't save it to database.
+            user = form.save()
+
+            # cleaned (normalized) data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.username = 'Ramesh'
+            user.set_password(password)
+
+            #This will save user to database
+            user.save()
+
+            #returns User Objects if credentials are correct.
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('music:index')
+
+        return render(request, self.template_name, {'form': form})
